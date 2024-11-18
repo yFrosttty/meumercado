850f2fe5cb348046a80b61181888113783d45394
GregoryVelosoS <123413549+GregoryVelosoS@users.noreply.github.com>
2024-11-18 11:19:44 -0300

    Adicionando pagina de login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,41 @@
+// importando components do bootstrap
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+
+const Login = () => {
+  return (
+    <div>
+      <Container
+        style={{ background: "dodgerblue", height: "100vh" }}
+        className="justify-content-center align-content-center"
+      >
+        {/* Icone de login */}
+        <span
+          style={{ fontSize: "200px", color: "white" }}
+          className="material-symbols-outlined"
+        >
+          login
+        </span>
+
+        {/* Caixinha de email */}
+        <FloatingLabel controlId="floatingInput" label="Email" className="mb-3">
+          <Form.Control type="email" placeholder="name@example.com" />
+        </FloatingLabel>
+
+        {/* Caixinha de senha */}
+        <FloatingLabel controlId="floatingPassword" label="Senha">
+          <Form.Control type="password" placeholder="Password" />
+        </FloatingLabel>
+
+        {/* Botao pra enviar o formulário */}
+        <Button variant="light" className="mt-4" size="lg">
+          Login
+        </Button>
+      </Container>
+    </div>
+  );
+};
+
+export default Login;
